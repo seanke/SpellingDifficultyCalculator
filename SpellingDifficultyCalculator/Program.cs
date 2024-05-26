ed09b480362125f4931b194f6d4baf1240e4d392
Sean Kelly <seankelly@outlook.co.nz>
2024-05-27 08:45:38 +1200

    Initial commit

---
+++ SpellingDifficultyCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿using SpellingDifficultyCalculator;
+
+Console.WriteLine("Enter a word to calculate its difficulty:");
+
+while (true)
+{
+    var word = Console.ReadLine();
+    var difficulty = WordDifficultyCalculator.CalculateDifficulty(word);
+    Console.WriteLine($"The difficulty of the word '{word}' is: {difficulty}");
+}
